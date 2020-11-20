6a27da817b5ae458c06d5e8f63c6898d4c4f3227
Mendel-light <mek397light@gmail.com>
2020-11-20 14:26:42 +0200

    Add Footer Down

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Main from "./components/Main";
 import Footer from "./components/Footer";
+import FooterMendel from "./components/FooterMendel";
 import Header from "./components/Header";
 
 
@@ -10,6 +11,7 @@ function App() {
             <Header/>
             <Main/>
             <Footer/>
+            <FooterMendel/>
         </div>
     );
 }
--- src/components/FooterMendel.js
+++ src/components/FooterMendel.js
@@ -1,9 +1,31 @@
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import imgFaceBook from "../images/icon-facebook.svg.png"
+import imgTwitter from "../images/icon-twitter.svg.png"
+import imgYoutube from "../images/icon-youtube.svg.png"
+import imgHtmpAcademy from "../images/htmlacademy.svg.png"
 
 const FooterMendel = () => {
     return (
-        <div>
-            
+        <div className={"mendel1Foot"}>
+    <div className={"mendel1Hashteg"}>
+        #visitSEDONA
+    </div>
+            <div className={"mendel1Wrapper-ico"}>
+                <a href={"https://www.facebook.com/"} className={"mendel1Icons"}>
+                    <img src={imgFaceBook} className={'mendel1Ico'}/>
+                </a>
+                <a href={"https://twitter.com/"} className={"mendel1Icons"}>
+                    <img src={imgTwitter} className={'mendel1Ico'}/>
+                </a>
+                <a href={"https://www.youtube.com/"} className={"mendel1Icons"}>
+                    <img src={imgYoutube} className={'mendel1Ico'}/>
+                </a>
+            </div>
+            <div className={'mendel1Wrapper-logo'}>
+                <p className={'mendel1Ico'}>Разработано</p>
+                <img src={imgHtmpAcademy}/>
+            </div>
         </div>
     );
 };
