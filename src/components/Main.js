8eed2271c989bbcbe20d4b20688fc04f9ed3d1ff
Pavel Kratser <kratserpavel@gmail.com>
2020-11-22 00:45:44 +0200

    eradicate catalog-page from main

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,15 +2,13 @@ import React from 'react';
 import DescriptionLayout from "./descriprion/description-layout";
 import CtaLayout from "./call-to-action/cta-layout";
 import Map from "./map/map";
-import CatalogPage from "./catalog-page/Catalog-page";
 
 const Main = () => {
     return (
         <div>
-            {/*<DescriptionLayout/>*/}
-            {/*<CtaLayout/>*/}
-            {/*<Map/>*/}
-            <CatalogPage/>
+            <DescriptionLayout/>
+            <CtaLayout/>
+            <Map/>
         </div>
     );
 };
